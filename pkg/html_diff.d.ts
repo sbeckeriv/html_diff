2767abd76cdebdc5cb7c111811cbdefd2eaf68c7
becker <github@deathbyescalator.com>
2020-08-11 21:51:19 -0700

    pkg

---
+++ pkg/html_diff.d.ts
@@ -0,0 +1,36 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} current 
+* @param {string} old 
+* @param {DiffConfig | undefined} config 
+* @returns {string} 
+*/
+export function diff(current: string, old: string, config?: DiffConfig): string;
+/**
+*/
+export class DiffConfig {
+  free(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_diffconfig_free: (a: number) => void;
+  readonly diff: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+        
